deae2804535cf6c6cbb44f4f584d94b143dc0dd2
refcell <abigger87@gmail.com>
2024-08-22 17:52:42 -0400

    fix(kona-providers): fmts

--- crates/kona-providers/src/lib.rs
+++ crates/kona-providers/src/lib.rs
@@ -1,8 +1,6 @@
 //! Provider implementations for Kona trait abstractions
 
-#![doc(
-    issue_tracker_base_url = "https://github.com/paradigmxyz/op-rs/issues/"
-)]
+#![doc(issue_tracker_base_url = "https://github.com/paradigmxyz/op-rs/issues/")]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 #![cfg_attr(not(any(test, feature = "online")), no_std)]
