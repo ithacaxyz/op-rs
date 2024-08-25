471a3a18a3879eeda15d69a869536ae42e306bc5
refcell <abigger87@gmail.com>
2024-08-25 13:58:59 -0400

    feat(net): Networking Crate

---
+++ crates/net/src/lib.rs
@@ -0,0 +1,10 @@
+//! Consensus Networking Library
+
+#![doc(issue_tracker_base_url = "https://github.com/paradigmxyz/op-rs/issues/")]
+#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+#![cfg_attr(not(test), no_std)]
+
+extern crate alloc;
+
+pub mod types;
