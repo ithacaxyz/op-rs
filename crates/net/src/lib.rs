d841275cb632b587c743e82db2b7366ed1f351f8
refcell <abigger87@gmail.com>
2024-08-26 16:50:51 -0400

    feat(net): Usage Docs (#41)

--- crates/net/src/lib.rs
+++ crates/net/src/lib.rs
@@ -1,5 +1,4 @@
-//! Consensus Networking Library
-
+#![doc = include_str!("../README.md")]
 #![doc(issue_tracker_base_url = "https://github.com/paradigmxyz/op-rs/issues/")]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
