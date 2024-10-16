1e5b6d63e250f6efdf472277578c7f55028a83ff
refcell <abigger87@gmail.com>
2024-10-16 15:33:13 -0400

    feat(ser): Trait Abstractions (#76)
    ### Description
    
    Re-exports trait abstractions from the
    [`op-alloy-rpc-jsonrpsee`](https://crates.io/crates/op-alloy-rpc-jsonrpsee)
    crate.
    
    Makes progress on https://github.com/paradigmxyz/op-rs/issues/20

--- crates/ser/src/traits/mod.rs
+++ crates/ser/src/traits/mod.rs
@@ -2,3 +2,9 @@
 
 pub mod compressor;
 pub use compressor::Compressor;
+
+// Re-export op-alloy-rpc-jsonrpsee crates
+pub use op_alloy_rpc_jsonrpsee::traits::{
+    OpAdminApiClient, OpAdminApiServer, OpP2PApiClient, OpP2PApiServer, RollupNodeClient,
+    RollupNodeServer,
+};
