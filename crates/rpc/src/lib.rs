3421efd7b374b08e0a04906bd5b72576669d9163
refcell <abigger87@gmail.com>
2024-10-17 16:07:45 -0400

    fix: issue link

--- crates/rpc/src/lib.rs
+++ crates/rpc/src/lib.rs
@@ -1,6 +1,6 @@
 //! Consensus RPC for Rollup Nodes
 
-#![doc(issue_tracker_base_url = "https://github.com/paradigmxyz/op-rs/issues/")]
+#![doc(issue_tracker_base_url = "https://github.com/ithacaxyz/op-rs/issues/")]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(test), warn(unused_crate_dependencies))]
 
